92a0341985c2b6cc9091e481e236fde30734000d
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-08-20 21:21:54 -0300

    Trabalhando com componentes de classe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component{
+  /* constructor(props){
+    super(props); */
+    /* this.handlePClick = this.handlePClick.bind(this);/* Passa o this para dentro do método */
+  state = {
+    name:  'Lucas Braz',
+    counter: 0
+  };
+  /* } */
+
+  handlePClick = () => {
+    this.setState({name: 'Junião'});
+  }
+
+  handleAClick = (event) => {/* arrow function busca o this no elemento pai */
+    event.preventDefault();
+    const {counter} = this.state;
+    console.log(counter)
+    this.setState({counter: counter+1});
+  }
+
+  render(){
+    const {name, counter} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +68,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
