4aa30ecb2f205776e2c54845a0d564c014411707
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-08-24 22:03:31 -0300

    Utilizando hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,13 +1,80 @@
 import "./styles.css";
 
-import { Component } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { loadPosts } from "../../utils/load-posts";
 import { Posts } from "../../components/Posts";
 import { Button } from "../../components/Button";
 import { TextInput } from "../../components/TextInput";
 
-class Home extends Component{
-  state = {/* Estado inicial */
+export const Home = () =>{
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPages] = useState(6);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPages >= allPosts.length? true : false;
+
+  const filterPosts = !!searchValue ? allPosts.filter(post =>{
+    return post.title.toLowerCase().includes(searchValue.toLowerCase())
+  }) : posts;
+
+  
+  const handleLoadPosts = useCallback(async (page, postsPerPages) =>{
+    const postsAndPhotos = await loadPosts();
+    
+    setPosts(postsAndPhotos.slice(page, postsPerPages));
+    setAllPosts(postsAndPhotos,)
+  }, [])
+
+  useEffect(()=>{
+    handleLoadPosts(0, postsPerPages);
+  }, [handleLoadPosts, postsPerPages]);//Array de dependências, sempre que a variável no array mudar o componente é re-renderizado
+
+  const loadMorePosts = () =>{
+    const nextPage = page + postsPerPages;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPages);
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+  }
+
+  const handleChange = event =>{
+    const {value} = event.target;
+    //this.setState({searchValue: value})
+    setSearchValue(value);
+  }
+
+  return (
+    <section className="container">
+      <div className="searchInput">
+        {!!searchValue && <h1>Pesquisar: {searchValue}</h1>}{/* Se não Tiver não busca não renderiza */}
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
+      {filterPosts.length > 0 && (
+         <Posts posts={filterPosts}/>
+      )}
+      {filterPosts.length === 0 && (
+         <p>Não existe post com essa informação</p>
+      )}
+    
+     <div className="button-container">
+      {!searchValue && (/* Se tiver busca não renderiza */
+         <Button 
+         disabled={noMorePosts}
+         text="Load More Posts"
+         onClick={loadMorePosts}
+         />
+      )}
+     
+     </div>
+    </section>
+  );
+}
+/* 
+class Home2 extends Component{
+  state = {// Estado inicial 
     posts: [],
     allPosts: [],
     page: 0,
@@ -15,15 +82,15 @@ class Home extends Component{
     searchValue: ''
   };
   
-  /* Ciclos de vida de components */
-  async componentDidMount(){/* Ocorre depois da criação do componente em tela */
+   Ciclos de vida de components 
+  async componentDidMount(){// Ocorre depois da criação do componente em tela 
     await this.loadPosts();
   }
-  /* 
+  
   componentDidUpdate(){ // Ocorre após a atualização do componente 
   }
   componentWillUnmount(){// Limpa os lixos deixados do componente  
-  } */
+  } 
   
   loadPosts = async () =>{
     const {page, postsPerPages} = this.state;
@@ -63,18 +130,18 @@ class Home extends Component{
     return (
       <section className="container">
         <div className="searchInput">
-          {!!searchValue && <h1>Pesquisar: {searchValue}</h1>}{/* Se não Tiver não busca não renderiza */}
+          {!!searchValue && <h1>Pesquisar: {searchValue}</h1>}{ //Se não Tiver não busca não renderiza }
           <TextInput searchValue={searchValue} handleChange={this.handleChange} />
         </div>
         {filterPosts.length > 0 && (
            <Posts posts={filterPosts}/>
         )}
         {filterPosts.length === 0 && (
-           <p>Não tem mais kkkkk...</p>
+           <p>Não existe post com essa informação</p>
         )}
       
        <div className="button-container">
-        {!searchValue && (/* Se tiver busca não renderiza */
+        {!searchValue && (// Se tiver busca não renderiza 
            <Button 
            disabled={noMorePosts}
            text="Load More Posts"
@@ -88,4 +155,4 @@ class Home extends Component{
   
   }
 }
-export default Home;
+export default Home; */
