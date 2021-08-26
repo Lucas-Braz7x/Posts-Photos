2091742d5e537f8e0362592df7834c843b56ff03
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-08-26 19:42:32 -0300

    Ajusta a importação do componente Home

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global-styles.css';
 
-import Home from './templates/Home';
+import {Home} from './templates/Home'
 
 ReactDOM.render(
   <React.StrictMode>
