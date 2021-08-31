63eaa3e8c0e8783c3fd68b553234a37beafd7ea7
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-08-31 20:16:45 -0300

    Cria testes para o componente TextInput

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,54 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {TextInput} from '.';
+/* 
+const props = {
+  posts: [
+    {
+      id: 1,
+      title: 'titulo 1',
+      body: 'body 1',
+      cover: 'img/img1.png'
+    },
+    {
+      id: 1,
+      title: 'titulo 2',
+      body: 'body 2',
+      cover: 'img/img2.png'
+    },
+    {
+      id: 3,
+      title: 'titulo 3',
+      body: 'body 3',
+      cover: 'img/img3.png'
+    }
+  ]
+} 
+ */
+describe('<TextInput />', () =>{
+  it('should have a value of searchValue', () =>{
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn} searchValue={'testando'}/>);
+  
+    const input = screen.getByPlaceholderText(/Digite o que você procura/i);
+    expect(input).toBeInTheDocument();
+    expect(input.value).toBe('testando')
+  });
+
+  it('should call handleChange function on each key pressed', () =>{
+    const fn = jest.fn();
+    render(<TextInput handleChange={fn}/>);
+
+    const input = screen.getByPlaceholderText(/Digite o que você procura/i);
+    const value = 'o valor';
+
+    userEvent.type(input, value);/* Verifica se o valor do input é igual ao valor esperado */
+    expect(fn).toHaveBeenCalledTimes(value.length);/* Verifica se fn foi chamado de acordo com o tamanho de value */
+  });
+  it('should match snapshot', () =>{
+    const fn = jest.fn();
+    const {container} = render(<TextInput handleChange={fn} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  }); 
+});
