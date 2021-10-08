20b38344bf3030d22641c24c1dd610346520815d
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-10-08 20:36:44 -0300

    Padronizando com o prettier

--- src/utils/load-posts.js
+++ src/utils/load-posts.js
@@ -1,16 +1,14 @@
-export const loadPosts = async () =>{
+export const loadPosts = async () => {
+  const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+  const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-  const postResponse = fetch("https://jsonplaceholder.typicode.com/posts");
-  const photoResponse = fetch("https://jsonplaceholder.typicode.com/photos");
-    
   const [posts, photos] = await Promise.all([postResponse, photoResponse]);
-    
+
   const postsJson = await posts.json();
   const photoJson = await photos.json();
 
   const postsAndPhotos = postsJson.map((post, index) => {
-    return{...post, cover: photoJson[index].url};
+    return { ...post, cover: photoJson[index].url };
   });
   return postsAndPhotos;
-}
-    
+};
