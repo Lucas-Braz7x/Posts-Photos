6276b0ba85159c1cc48139297cdaa1324a9d77cc
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-10-09 22:04:55 -0300

    Intercepta requisições com Mock Service Worker (msw) para a Home

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,14 +1,59 @@
-/* import { render, screen } from '@testing-library/react';
-import App from './Home';
- */
-/* test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
- */
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Home } from '.';
+
+const handlers = [
+  /* Cria um mini servidor para olhar as requisições */
+  rest.get('*https://jsonplaceholder.typicode.com/posts*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          img: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          img: 'img2.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          img: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a Dummy test', () => {
-    expect(1).toBe(1);
+  /* Liga o servidor */
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  /* Desliga o servidor */
+  afterAll(() => {
+    server.close();
+  });
+  it('should render search, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existe post com essa informação');
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
